aabfb42d83ae44e497ee2f76d0212c1023409a0e
Ruben Barros <46852305+SASNOVACAT08@users.noreply.github.com>
2021-11-09 16:26:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $url = "https://api.binance.com/api/v3/ticker/price";
 $curl = curl_init($url);
 
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$curl = curl_exec($curl)
-$curl_close($curl)
+$curl = curl_exec($curl);
+$curl_close($curl);
    
-echo($curl)
+echo($curl);
